404387af86e4a9f6d0bf86f05b342996a03a2e17
6YanIS9 <123742387+6YanIS9@users.noreply.github.com>
2025-01-29 17:16:39 +0100

    D

--- traitement.py
+++ traitement.py
@@ -74,3 +74,23 @@ conn.commit()
 conn.close()
 
 print("Base de données 'infractions.db' créée avec succès!")
+
+# Charger le fichier Excel
+file_path = "/crimes-et-delits-enregistres-par-les-services-de-gendarmerie-et-de-police-depuis-2012.xlsx"
+xls = pd.ExcelFile(file_path)
+
+# Créer un dossier pour stocker les fichiers découpés
+output_dir = "/decoupage_fichiers"
+os.makedirs(output_dir, exist_ok=True)
+
+# Récupérer les noms des feuilles en ignorant la première
+sheet_names = xls.sheet_names[1:]
+
+# Parcourir les feuilles restantes et sauvegarder chaque feuille en CSV
+for sheet_name in sheet_names:
+    df = pd.read_excel(xls, sheet_name=sheet_name)
+    output_file = os.path.join(output_dir, f"{sheet_name}.csv")
+    df.to_csv(output_file, index=False, encoding='utf-8')
+    print(f"Fichier enregistré : {output_file}")
+
+print("Découpage terminé !")
